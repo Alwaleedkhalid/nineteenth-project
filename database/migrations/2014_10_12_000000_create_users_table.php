0b5c3a8debeb4b9faf2fdbd701010bfa2f6d44b4
Alwaleed <alwaleed.alageel@gmail.com>
2020-02-11 03:33:04 +0300

    c

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('employee')->nullable(); // role for auth 
+            $table->string('role')->default('Viwe')->nullable(); // role for auth 
             $table->rememberToken();
             $table->timestamps();
         });
