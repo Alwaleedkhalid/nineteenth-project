f86b491b3023000e65003a4d34f2fef36acf1ae9
Alwaleed <alwaleed.alageel@gmail.com>
2020-02-11 15:24:35 +0300

    ..

--- resources/views/employees/employee.blade.php
+++ resources/views/employees/employee.blade.php
@@ -1,166 +1,77 @@
 @extends('layouts.app')
-
-<style>
-.max_length_description {
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  max-width: 200px;
-}
-</style>
-
 @section('content')
 
-<div class="row">
+<div class="card border-light mb-3 shadow-sm p-3 mb-5 bg-white rounded">
+  <a href="/employee/create" class="float-right  float-right icon button ui grey button">
+    <i class="add icon"></i>
+    Add New Employee
+  </a> 
+  <table class="table table-hover mt-5">
+    {{-- <caption class="">قائمة الموظفين</caption> --}}
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">E-Mail</th>
+        <th scope="col">Unit</th>
+        <th scope="col">Role</th>
+        <th scope="col" class="text-center"></th>
+      </tr>
+    </thead>
 
- <div class="col-5">
-    {{-- <div class="card border-light mb-3 shadow-sm p-3 mb-5 bg-white rounded">
-       <hr> 
-       <h4 class="text-center">CHARTS</h4>
-    </div>
-
-    <div class="card border-light mb-3 shadow-sm p-3 mb-5 bg-white rounded">
-      <hr>
-      <h4 class="text-center">CHARTS</h4>
-
-    </div> --}}
-
-    <div class="card border-light mb-3 shadow-sm p-3 mb-5 bg-white rounded">
-
-      <a href="/employee/create" class="float-right ui teal labeled icon button">
-        Add New Employee
-        <i class="add icon"></i>
-      </a>
-  
-    </div>
-
- </div>
- <div class="col-7">
-    <div class="card border-light mb-3 shadow-sm p-3 mb-5 bg-white rounded">
-      <form method="get" action="/search" role="search">
-        <div class="ui icon input d-flex justify-content-around">
-              <input name="search" placeholder="Search..." type="search">
-              <i class="circular search link icon"></i>
-          </div>
-      </form>
-      <hr>
-      <div class="row">
-      {{-- @foreach ($report as $reports)
-      <div class="col">
-      <div class="card shadow ui raised link card pl-1 mt-2 ml-1" onclick="location.href='report/{{$reports->id}}'" style="width: 20rem;">
-        <div class="card-body">
-            <h5 class="card-title">{{strtoupper($reports->title)}}</h5>
-            @if($reports->created_at == $reports->updated_at)
+    <tbody>
+      {{-- @foreach ($user as $key => $users) --}}
+      <tr>
             
-              <small class="text-muted float-right">{{($reports->created_at->format('d M y : h'))}}</small>
-            
-            @else
-  
-              <small class="text-muted float-right" style="background:#ff075714;">{{($reports->created_at->format('d M y : h'))}}</small>
-               
-            @endif
-          <h6 class="card-subtitle mb-2 text-muted">{{ucfirst($reports->user->name)}}</h6>
-        <p class="card-text max_length_description">{{ucfirst($reports->description)}}</p>
-        <hr>
-        <a href="report/{{$reports->id}}" class="card-link">More    </a><i class="fas fa-chevron-right"></i>
-          @if ($reports->status == 'in process')
-            <div class="spinner-border spinner-border-sm text-warning float-right" role="status">
-              <span class="sr-only">Loading...</span>
+        {{-- <th scope="row">{{$key+1}}</th> --}}
+        {{-- <td>{{strtoupper($users->name)}}</td> --}}
+        {{-- <td>{{$users->email}}</td> --}}
+        {{-- <td>{{strtoupper($users->unit_id)}}</td> --}}
+        {{-- <td>{{strtoupper($users->role)}}</td> --}}
+
+        <td>
+
+          <div class="row">
+            <div class="col-6">
+              {{-- <form action="{{ action('AdminController@show' , $users->id) }}" method="GET"> --}}
+              <a   href="admin-panel" type="button" class="btn btn-secondary btn-sm font-weight-bold ">Report <i class="far fa-folder-open"></i></a>
+              {{-- </form> --}}
             </div>
-          @elseif($reports->status == 'rejected')
-            <div class="float-right">
-              <i class="fas fa-times-circle text-danger"></i>
+            <div class="col-3 clearfix">
+              <button type="button" class="btn btn-primary btn-sm center-block font-weight-bold float-right"><i class="fas fa-user-edit"></i></button>
             </div>
-          @else
-            <div class="float-right">
-              <i class="fas fa-check-circle text-success"></i>
+            <div class="col-3">
+              {{-- <form action="{{action('AdminController@destroy' , $users->id)}}" method="post">  --}}
+                  {{-- @csrf --}}
+                  {{-- @method('DELETE') --}}
+                <button class="btn btn-danger btn-sm font-weight-bold float-right" type="submit"><i class="fas fa-user-times"></i></button>
+              {{-- </form> --}}
             </div>
-          @endif
-          
-        </div>
-      </div>
-    </div>
-      @endforeach
-    </div>
-    <hr>
-    <div class="m-1 align-self-center">
-    <b>{{$report->links("pagination::bootstrap-4")}}</b>
-    </div>
-    </div>
-</div>
-</div> --}}
+          </div>  
 
-{{-- <hr>
+        </td>
 
-<div class="row">
-  <div class="col-8">
-    <form method="get" action="/search" role="search">
-      <div class="ui icon input">
-          <input name="search" placeholder="Search..." type="search">
-          <i class="circular search link icon"></i>
-      </div>
-    </form>
-  </div>
-  <div class="col-4">
-    <a href="/report/create" class="float-right ui teal labeled icon button">
-      Create New Report
-      <i class="add icon"></i>
-    </a>
-  </div>
-</div>
-<div class="card border-light pb-4 shadow-sm pl-5 bg-white rounded" style="width: 50rem;">
-<div class="row">
+      </tr>
+      {{-- @endforeach --}}
 
-  @foreach ($report as $reports)
-<div class="col">
-  <div class="card shadow ui raised link card p-1 mt-3" onclick="location.href='report/{{$reports->id}}'" style="width: 20rem;">
-      <div class="card-body">
-          <h5 class="card-title">{{strtoupper($reports->title)}}</h5>
-          @if($reports->created_at == $reports->updated_at)
-          
-            <small class="text-muted float-right">{{($reports->created_at->format('d M y : h'))}}</small>
-          
-          @else
+     
+      
+      {{-- <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Larry</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+        <td>@twitter</td>
+      </tr> --}}
+    </tbody>
+  </table>
+</div>
 
-            <small class="text-muted float-right" style="background:#ff075714;">{{($reports->created_at->format('d M y : h'))}}</small>
-             
-          @endif
-        <h6 class="card-subtitle mb-2 text-muted">{{ucfirst($reports->user->name)}}</h6>
-      <p class="card-text max_length_description">{{ucfirst($reports->description)}}</p>
-      <hr>
-      <a href="report/{{$reports->id}}" class="card-link">More    </a><i class="fas fa-chevron-right"></i>
-        @if ($reports->status == 'in process')
-          <div class="spinner-border spinner-border-sm text-warning float-right" role="status">
-            <span class="sr-only">Loading...</span>
-          </div>
-        @elseif($reports->status == 'rejected')
-          <div class="float-right">
-            <i class="fas fa-times-circle text-danger"></i>
-          </div>
-        @else
-          <div class="float-right">
-            <i class="fas fa-check-circle text-success"></i>
-          </div>
-        @endif
-        
-      </div>
-    </div>
-  </div>
-  
-@endforeach
-</div>
-</div>
-<div class="mt-5">
- <b>{{$report->links("pagination::bootstrap-4")}}</b>
-</div> --}}
 @endsection
-
-<script>
-
-  $("div").click(function(){
-   window.location=$(this).find("a").attr("href"); return false;
-  });
-
-</script>
-
-  
